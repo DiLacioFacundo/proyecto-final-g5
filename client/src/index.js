8841a9d41e390dc65e472b8733e02bc67688c129
Facundo Di Lacio <facundodilacio.esc13@gmail.com>
2021-11-26 13:28:28 -0300

    26/11/2021

--- client/src/index.js
+++ client/src/index.js
@@ -1,7 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
+// import './index.css';
 import App from './App';
+import 'bootstrap/dist/css/bootstrap.css';
+// Put any other imports below so that CSS from your
+// components takes precedence over default syles
 
 ReactDOM.render(
   <React.StrictMode>
